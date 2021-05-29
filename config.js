9791735f3a51a777a1a81775a0b7f8752deb4407
Sachin Virdy <svirdy1@gmail.com>
2021-05-29 17:08:38 -0400

    Identity

---
+++ config.js
@@ -0,0 +1,10 @@
+module.exports = {
+    plugins: [
+        {
+          resolve: `gatsby-plugin-netlify-identity`,
+          options: {
+            url: `https://sachintest.netlify.app/`
+          }
+        }
+    ]
+}
