529ac43746e166ef3d6a98437e328ea6e9a10bd7
Sachin Virdy <svirdy1@gmail.com>
2021-05-29 16:49:58 -0400

    First push

---
+++ functions/delete-testimonial.js
@@ -0,0 +1,30 @@
+// delete-testimonial.js
+
+const query = require("./utils/query");
+
+const DELETE_TESTIMONIAL = `
+  mutation($id: ID!) {
+    deleteMessage(id: $id){
+      _id
+    }
+  }
+`;
+
+exports.handler = async event => {
+  const { id } = JSON.parse(event.body);
+  const { data, errors } = await query(
+        DELETE_TESTIMONIAL, { id });
+
+  if (errors) {
+    return {
+      statusCode: 500,
+      body: JSON.stringify(errors)
+    };
+  }
+
+  return {
+    statusCode: 200,
+    body: JSON.stringify({ deletedMessage: data.deleteMessage 
+   })
+  };
+};
